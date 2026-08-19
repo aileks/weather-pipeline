6cd42fbe62f60808ea0f3220472807cad1f54a1a
Caspian Harper <caspian@aileks.dev>
2026-08-19 19:24:31 -0400

    add dbt project skeleton with clean schema names

---
+++ dbt/macros/generate_schema_name.sql
@@ -0,0 +1,8 @@
+{# Schemas appear exactly as declared (staging, core, marts): no target prefix. #}
+{% macro generate_schema_name(custom_schema_name, node) -%}
+    {%- if custom_schema_name is none -%}
+        {{ target.schema }}
+    {%- else -%}
+        {{ custom_schema_name | trim }}
+    {%- endif -%}
+{%- endmacro %}
